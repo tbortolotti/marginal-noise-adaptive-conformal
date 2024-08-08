db1b48e0d54de4b2a4a8ba6a8368aac9184a5811
tbortolotti <teresa.bortolotti1@gmail.com>
2024-08-07 17:18:25 -0700

    added file for label conditional

--- experiments/make_plots.R
+++ experiments/make_plots.R
@@ -42,13 +42,14 @@ linetype.scale <- c(1,1)
 
 #' Plot 1: Finite sample correction as a function of the calibration set size,
 #'         for different number of classes
-make_figure_A1 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1) {
+make_figure_A1 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, model_name="B",
+                           method.values, method.labels, label.values, label.labels) {
   if(reload) {
     summary <- load_data(1)
   }
   
   df <- summary %>%
-    filter(plot=="Klab", epsilon==plot.epsilon, model_name=="B", K %in% label.values) %>%
+    filter(plot=="Klab", epsilon==plot.epsilon, model_name==model_name, K %in% label.values) %>%
     mutate(Method = factor(Method, levels = method.values, labels = method.labels)) %>%
     mutate(K = factor(K, levels = label.values, labels = label.labels))
   
@@ -67,7 +68,7 @@ make_figure_A1 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1) {
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
   
   if(save_plots) {
-    plot.file <- sprintf("figures/delta_marg_const_B_Klab_eps%f.pdf", plot.epsilon)
+    plot.file <- sprintf("figures/delta_marg_const_%s_Klab_eps%f.pdf", model_name, plot.epsilon)
     ggsave(file=plot.file, height=3, width=7, units="in")
     return(NULL)
   } else{
@@ -77,18 +78,21 @@ make_figure_A1 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1) {
 
 label.values = c(4, 8, 16)
 label.labels = c("K=4", "K=8", "K=16")
+model.name = "B"
+plot.epsilon = 0.1
 
-make_figure_A1(plot.epsilon=0.1, save_plots=TRUE, reload=FALSE)
+make_figure_A1(plot.epsilon=plot.epsilon, model_name=model_name, method.values=method.values, method.labels=method.labels,
+               label.values=label.values, label.labels=label.labels, save_plots=TRUE, reload=FALSE)
 
 #' Plot 2: Finite sample correction as a function of the number of classes, for different
 #'         values of the calibration set size
-make_figure_A2 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1) {
+make_figure_A2 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, model_name="B") {
   if(reload) {
     summary <- load_data(1)
   }
   
   df <- summary %>%
-    filter(plot=="nlab", epsilon==plot.epsilon, model_name=="B", n_cal %in% label.values) %>%
+    filter(plot=="nlab", epsilon==plot.epsilon, model_name==model_name, n_cal %in% label.values) %>%
     mutate(Method = factor(Method, levels = method.values, labels = method.labels)) %>%
     mutate(n_lab = factor(n_cal, levels = label.values, labels = label.labels))
   
@@ -105,7 +109,7 @@ make_figure_A2 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1) {
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
   
   if(save_plots) {
-    plot.file <- sprintf("figures/delta_marg_const_B_nlab_eps%f.pdf", plot.epsilon)
+    plot.file <- sprintf("figures/delta_marg_const_%s_nlab_eps%f.pdf", model_name, plot.epsilon)
     ggsave(file=plot.file, height=3, width=7, units="in")
     return(NULL)
   } else{
@@ -115,8 +119,10 @@ make_figure_A2 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1) {
 
 label.values = c(100, 500, 1000)
 label.labels = c("n=100", "n=500", "n=1000")
+plot.epsilon = 0.1
+model_name = "B"
 
-make_figure_A2(plot.epsilon=0.1, save_plots=TRUE, reload=FALSE)
+make_figure_A2(plot.epsilon=plot.epsilon, model_name=model_name, save_plots=TRUE, reload=FALSE)
 
 ### Two-level randomized response model ----------------------------------------
 ### Plot of the finite sample correction for the optimized betas and the rr betas
@@ -152,15 +158,15 @@ color.scale <- cbPalette[c(3,7)]
 shape.scale <- c(2,0)
 linetype.scale <- c(1,1)
 
-#' Plot 1: Finite sample correction as a function of the calibration set size,
+#' Plot 3: Finite sample correction as a function of the calibration set size,
 #'         for different number of classes
-make_figure_A1 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
+make_figure_A3 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2, model_name="B") {
   if(reload) {
     summary <- load_data(1)
   }
   
   df <- summary %>%
-    filter(plot=="Klab", epsilon==plot.epsilon, nu==plot.nu, model_name=="BRR") %>%
+    filter(plot=="Klab", epsilon==plot.epsilon, nu==plot.nu, model_name==model_name) %>%
     filter(K %in% c(4, 8, 16)) %>%
     mutate(Method = factor(Method, levels = method.values, labels = method.labels)) %>%
     mutate(K = factor(K, levels = label.values, labels = label.labels))
@@ -180,7 +186,7 @@ make_figure_A1 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plo
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
   
   if(save_plots) {
-    plot.file <- sprintf("figures/delta_marg_const_BRR_Klab_eps%f_nu%f.pdf", plot.epsilon, plot.nu)
+    plot.file <- sprintf("figures/delta_marg_const_%s_Klab_eps%f_nu%f.pdf", model_name, plot.epsilon, plot.nu)
     ggsave(file=plot.file, height=3, width=7, units="in")
     return(NULL)
   } else{
@@ -190,18 +196,21 @@ make_figure_A1 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plo
 
 label.values = c(4, 8, 16)
 label.labels = c("K=4", "K=8", "K=16")
+plot.epsilon = 0.1
+plot.nu = 0.2
+model_name = "BRR"
 
-make_figure_A1(plot.epsilon=0.1, plot.nu=0.2, save_plots=TRUE, reload=FALSE)
+make_figure_A3(plot.epsilon=plot.epsilon, plot.nu=plot.nu, model_name=model_name, save_plots=TRUE, reload=FALSE)
 
-#' Plot 2: Finite sample correction as a function of the number of classes, for different
+#' Plot 4: Finite sample correction as a function of the number of classes, for different
 #'         values of the calibration set size
-make_figure_A2 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
+make_figure_A4 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2, model_name="B") {
   if(reload) {
     summary <- load_data(1)
   }
   
   df <- summary %>%
-    filter(plot=="nlab", epsilon==plot.epsilon, nu==plot.nu, model_name=="BRR",
+    filter(plot=="nlab", epsilon==plot.epsilon, nu==plot.nu, model_name==model_name,
            n_cal %in% label.values) %>%
     mutate(Method = factor(Method, levels = method.values, labels = method.labels)) %>%
     mutate(n_lab = factor(n_cal, levels = label.values, labels = label.labels))
@@ -219,7 +228,7 @@ make_figure_A2 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plo
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
   
   if(save_plots) {
-    plot.file <- sprintf("figures/delta_marg_const_BRR_nlab_eps%f_nu%f.pdf", plot.epsilon, plot.nu)
+    plot.file <- sprintf("figures/delta_marg_const_%s_nlab_eps%f_nu%f.pdf", model_name, plot.epsilon, plot.nu)
     ggsave(file=plot.file, height=3, width=7, units="in")
     return(NULL)
   } else{
@@ -229,102 +238,12 @@ make_figure_A2 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plo
 
 label.values = c(100, 500, 1000)
 label.labels = c("n=100", "n=500", "n=1000")
+plot.epsilon = 0.1
+plot.nu = 0.2
+model_name = "BRR"
 
-make_figure_A2(plot.epsilon=0.1, plot.nu=0.2, save_plots=TRUE, reload=FALSE)
+make_figure_A4(plot.epsilon=plot.epsilon, plot.nu=plot.nu, model_name=model_name, save_plots=TRUE, reload=FALSE)
 
-
-#' Plot 3: Finite sample correction as a function of the calibration set size,
-#'         for different number of classes and for different combinations of epsilon and nu
-#'
-
-make_figure_A3 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon.vals=c(0.1, 0.5), plot.nu=0.2) {
-  if(reload) {
-    summary <- load_data(1)
-  }
-  
-  df <- summary %>%
-    filter(plot=="Klab", epsilon %in% plot.epsilon.vals, nu==plot.nu, model_name=="BRR",
-           K %in% label.values) %>%
-    mutate(Method = factor(Method, levels = method.values, labels = method.labels)) %>%
-    mutate(K = factor(K, levels = label.values, labels = label.labels)) %>%
-    mutate(epsilon = factor(epsilon, labels = paste0("epsilon=", plot.epsilon.vals)))
-  
-  y_min <- min(df$values, na.rm = TRUE)
-  y_max <- max(df$values, na.rm = TRUE)
-  
-  pp <- ggplot(df, aes(x = n_cal, y = values, color = Method, shape = Method, linetype = Method)) +
-    geom_point() +
-    geom_line() +
-    facet_grid(epsilon ~ K, scales = "fixed") +
-    #scale_x_log10(breaks = unique(df$n_cal), labels = unique(df$n_cal)) +
-    scale_x_continuous(trans = 'log10', breaks = unique(df$n_cal), labels = unique(df$n_cal)) +
-    scale_color_manual(values = color.scale) +
-    scale_shape_manual(values = shape.scale) +
-    scale_linetype_manual(values = linetype.scale) +
-    xlab("Number of calibration samples") +
-    ylab(expression(delta^"marg" * "(n)")) +
-    ylim(y_min, y_max) +
-    theme_bw() +
-    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
-  
-  if(save_plots) {
-    plot.file <- sprintf("figures/delta_marg_const_BRR_Klab_nu%f.pdf", plot.nu)
-    ggsave(file=plot.file, height=3, width=7, units="in")
-    return(NULL)
-  } else{
-    return(pp)
-  }
-}
-
-label.values = c(4, 8, 16)
-label.labels = c("K=4", "K=8", "K=16")
-
-make_figure_A3(plot.epsilon.vals=c(0.1,0.5), plot.nu=0.2, save_plots=TRUE, reload=FALSE)
-        
-#' Plot 4: Finite sample correction as a function of the number of classes, for different
-#'         values of the calibration set size and for different combinations of epsilon and nu
-#'         
-make_figure_A4 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon.vals=c(0.1,0.5), plot.nu=0.2) {
-  if(reload) {
-    summary <- load_data(1)
-  }
-  
-  df <- summary %>%
-    filter(plot=="nlab", epsilon %in% plot.epsilon.vals, nu==plot.nu, model_name=="BRR",
-           n_cal %in% label.values) %>%
-    mutate(Method = factor(Method, levels = method.values, labels = method.labels)) %>%
-    mutate(n_lab = factor(n_cal, levels = label.values, labels = label.labels)) %>%
-    mutate(epsilon = factor(epsilon, labels = paste0("epsilon=", plot.epsilon.vals)))
-  
-  y_min <- min(df$values, na.rm = TRUE)
-  y_max <- max(df$values, na.rm = TRUE)
-  
-  pp <- ggplot(df, aes(x = K, y = values, color = Method, shape = Method, linetype = Method)) +
-    geom_point() +
-    geom_line() +
-    facet_grid(epsilon ~ n_lab, scales = "fixed") +
-    scale_color_manual(values = color.scale) +
-    scale_shape_manual(values = shape.scale) +
-    scale_linetype_manual(values = linetype.scale) +
-    xlab("Number of classes") +
-    ylab(expression(delta^"marg" * "(n)")) +
-    ylim(y_min, y_max) +
-    theme_bw() +
-    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
-  
-  if(save_plots) {
-    plot.file <- sprintf("figures/delta_marg_const_BRR_nlab_nu%f.pdf", plot.nu)
-    ggsave(file=plot.file, height=3, width=7, units="in")
-    return(NULL)
-  } else{
-    return(pp)
-  }
-}
-
-label.values = c(50, 100, 500)
-label.labels = c("n=50", "n=100", "n=500")
-
-make_figure_A4(plot.epsilon.vals=c(0.1,0.5), plot.nu=0.2, save_plots=TRUE, reload=FALSE)
 
 #' Plot 5: Finite sample correction as a function of the calibration set size,
 #'         for different number of classes and for different combinations of epsilon and nu
@@ -421,6 +340,56 @@ label.labels = c("n=100", "n=500", "n=1000")
 
 make_figure_A6(plot.epsilon=0.1, plot.nu.vals=c(0.2, 0.8), save_plots=TRUE, reload=FALSE)
 
+#' Plot 7: Finite sample correction as a function of nu, for different
+#'         values of the calibration set size and for different number of classes
+#'         
+make_figure_A7 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.K.vals=c(4,16), model_name) {
+  if(reload) {
+    summary <- load_data(1)
+  }
+  df <- summary %>%
+    filter(plot=="nu_var", epsilon == plot.epsilon, K %in% plot.K.vals, model_name==model_name,
+           n_cal %in% label.values) %>%
+    mutate(Method = factor(Method, levels = method.values, labels = method.labels)) %>%
+    mutate(n_lab = factor(n_cal, levels = label.values, labels = label.labels)) %>%
+    mutate(K = factor(K, levels=plot.K.vals, labels = c("K=4","K=16")))
+  
+  y_min <- min(df$values, na.rm = TRUE)
+  y_max <- max(df$values, na.rm = TRUE)
+  
+  pp <- ggplot(df, aes(x = nu, y = values, color = Method, shape = Method, linetype = Method)) +
+    geom_point() +
+    geom_line() +
+    facet_grid(K ~ n_lab, scales = "fixed") +
+    scale_color_manual(values = color.scale) +
+    scale_shape_manual(values = shape.scale) +
+    scale_linetype_manual(values = linetype.scale) +
+    xlab(expression(nu)) +
+    ylab(expression(delta^"marg" * "(n)")) +
+    ylim(y_min, y_max) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+  
+  if(save_plots) {
+    plot.file <- sprintf("figures/delta_marg_const_BRR_nu_var_eps%f.pdf", plot.epsilon)
+    ggsave(file=plot.file, height=3, width=7, units="in")
+    return(NULL)
+  } else{
+    return(pp)
+  }
+}
+
+label.values = c(100, 500, 1000)
+label.labels = c("n=100", "n=500", "n=1000")
+#plot.epsilon = 0.1
+plot.epsilon = 0.5
+plot.K.vals = c(4,16)
+model_name = "BRR"
+
+make_figure_A7(plot.epsilon=plot.epsilon, plot.K.vals=plot.K.vals, model_name=model_name,
+               save_plots=TRUE, reload=FALSE)
+
+
 
 ### Comparison with the old finite sample correction ----------------------------------------
 ### Plot of the finite sample correction for the optimized betas, the rr betas and the old
@@ -455,9 +424,9 @@ color.scale <- cbPalette[c(3,7,8)]
 shape.scale <- c(2,0,1)
 linetype.scale <- c(1,1,1)
 
-#' Plot 7: Finite sample correction as a function of the calibration set size,
+#' Plot 9: Finite sample correction as a function of the calibration set size,
 #'         for different number of classes
-make_figure_A7 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
+make_figure_A9 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
   if(reload) {
     summary <- load_data(1)
   }
@@ -494,11 +463,11 @@ make_figure_A7 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plo
 label.values = c(4, 8, 16)
 label.labels = c("K=4", "K=8", "K=16")
 
-make_figure_A7(plot.epsilon=0.1, plot.nu="none", save_plots=TRUE, reload=FALSE)
+make_figure_A9(plot.epsilon=0.1, plot.nu="none", save_plots=TRUE, reload=FALSE)
 
-#' Plot 8: Finite sample correction as a function of the number of classes, for different
+#' Plot 10: Finite sample correction as a function of the number of classes, for different
 #'         values of the calibration set size
-make_figure_A8 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
+make_figure_A10 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
   if(reload) {
     summary <- load_data(1)
   }
@@ -533,12 +502,12 @@ make_figure_A8 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plo
 label.values = c(500, 1000, 2000)
 label.labels = c("n=500", "n=1000", "n=2000")
 
-make_figure_A8(plot.epsilon=0.1, plot.nu="none", save_plots=TRUE, reload=FALSE)
+make_figure_A10(plot.epsilon=0.1, plot.nu="none", save_plots=TRUE, reload=FALSE)
 
 
-#' Plot 9: Finite sample correction as a function of the calibration set size,
+#' Plot 11: Finite sample correction as a function of the calibration set size,
 #'         for different number of classes
-make_figure_A9 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
+make_figure_A11 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
   if(reload) {
     summary <- load_data(1)
   }
@@ -575,11 +544,11 @@ make_figure_A9 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plo
 label.values = c(4, 8, 16)
 label.labels = c("K=4", "K=8", "K=16")
 
-make_figure_A9(plot.epsilon=0.1, plot.nu=0.2, save_plots=TRUE, reload=FALSE)
+make_figure_A11(plot.epsilon=0.1, plot.nu=0.2, save_plots=TRUE, reload=FALSE)
 
-#' Plot 10: Finite sample correction as a function of the number of classes, for different
+#' Plot 12: Finite sample correction as a function of the number of classes, for different
 #'         values of the calibration set size
-make_figure_A10 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
+make_figure_A12 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu=0.2) {
   if(reload) {
     summary <- load_data(1)
   }
@@ -614,13 +583,13 @@ make_figure_A10 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, pl
 label.values = c(500, 1000, 2000)
 label.labels = c("n=500", "n=1000", "n=2000")
 
-make_figure_A10(plot.epsilon=0.1, plot.nu=0.2, save_plots=TRUE, reload=FALSE)
+make_figure_A12(plot.epsilon=0.1, plot.nu=0.2, save_plots=TRUE, reload=FALSE)
 
-#' Plot 11: Finite sample correction as a function of the calibration set size,
+#' Plot 13: Finite sample correction as a function of the calibration set size,
 #'         for different number of classes and for different combinations of epsilon and nu
 #'
 
-make_figure_A11 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu.vals=c(0.2,0.8)) {
+make_figure_A13 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu.vals=c(0.2,0.8)) {
   if(reload) {
     summary <- load_data(1)
   }
@@ -662,14 +631,14 @@ make_figure_A11 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, pl
 label.values = c(4, 8, 16)
 label.labels = c("K=4", "K=8", "K=16")
 
-make_figure_A11(plot.epsilon=0.1, plot.nu.vals=c(0.2,0.8), save_plots=TRUE, reload=FALSE)
+make_figure_A13(plot.epsilon=0.1, plot.nu.vals=c(0.2,0.8), save_plots=TRUE, reload=FALSE)
 
 
 
-#' Plot 12: Finite sample correction as a function of the number of classes, for different
+#' Plot 14: Finite sample correction as a function of the number of classes, for different
 #'         values of the calibration set size and for different combinations of epsilon and nu
 #'         
-make_figure_A12 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu.vals=c(0.2,0.8)) {
+make_figure_A14 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, plot.nu.vals=c(0.2,0.8)) {
   if(reload) {
     summary <- load_data(1)
   }
@@ -709,7 +678,7 @@ make_figure_A12 <- function(save_plots=FALSE, reload=FALSE, plot.epsilon=0.1, pl
 label.values = c(500, 1000, 2000)
 label.labels = c("n=500", "n=1000", "n=2000")
 
-make_figure_A12(plot.epsilon=0.1, plot.nu.vals=c(0.2, 0.8), save_plots=TRUE, reload=FALSE)
+make_figure_A14(plot.epsilon=0.1, plot.nu.vals=c(0.2, 0.8), save_plots=TRUE, reload=FALSE)
 
 
 # MAIN -------------------------------------------------------------------------
