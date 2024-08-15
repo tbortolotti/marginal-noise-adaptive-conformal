c8139a1677f5724c9f63e12e8da756c6aab9cd30
tbortolotti <teresa.bortolotti1@gmail.com>
2024-08-14 21:16:41 -0700

    re-ordering

---
+++ experiments/exp_1.py
@@ -0,0 +1,285 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import pandas as pd
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+import pdb
+
+import sys
+sys.path.append("..")
+sys.path.append("../third_party")
+
+
+from cln import data
+from cln import contamination
+from cln.utils import evaluate_predictions, estimate_rho
+
+from cln.classification import LabelNoiseConformal
+
+from third_party import arc
+
+
+# Define default parameters
+exp_num = 1
+data_name = 'synthetic1'
+num_var = 20
+K = 4
+signal = 1
+model_name = 'RFC'
+epsilon = 0.1
+nu = "None"
+contamination_model = "uniform"
+n_train = 1000
+n_cal = 5000
+estimate = "None"
+seed = 1
+
+# Parse input parameters
+if True:
+    print ('Number of arguments:', len(sys.argv), 'arguments.')
+    print ('Argument List:', str(sys.argv))
+    if len(sys.argv) != 13:
+        print("Error: incorrect number of parameters.")
+        quit()
+    sys.stdout.flush()
+
+    exp_num = int(sys.argv[1])
+    data_name = sys.argv[2]
+    num_var = int(sys.argv[3])
+    K = int(sys.argv[4])
+    signal = float(sys.argv[5])
+    model_name = sys.argv[6]
+    epsilon = float(sys.argv[7])
+    contamination_model = sys.argv[8]
+    n_train = int(sys.argv[9])
+    n_cal = int(sys.argv[10])
+    estimate = sys.argv[11]
+    seed = int(sys.argv[12])
+
+
+# Define other constant parameters
+n_test = 2000
+batch_size = 5
+allow_empty = True
+
+# Initialize the data distribution
+if data_name == "synthetic1":
+    data_distribution = data.DataModel_1(K, num_var, signal=signal, random_state=seed)
+elif data_name == "synthetic2":
+    data_distribution = data.DataModel_2(K, num_var, signal=signal, random_state=seed)
+elif data_name == "synthetic3":
+    data_distribution = data.DataModel_3(K, num_var, signal=signal, random_state=seed)
+else:
+    print("Unknown data distribution!")
+    sys.stdout.flush()
+    exit(-1)
+
+# Estimate the label proportions from the population model
+rho = data_distribution.estimate_rho()
+
+# Initialize noise contamination process
+if contamination_model == "uniform":
+    T = contamination.construct_T_matrix_simple(K, epsilon)  
+    M = contamination.convert_T_to_M(T,rho)
+elif contamination_model == "block":
+    T = contamination.construct_T_matrix_block(K, epsilon)
+    M = contamination.convert_T_to_M(T,rho)
+elif contamination_model == "RRB":
+    T = contamination.construct_T_matrix_block_RR(K, epsilon, nu)
+    M = contamination.convert_T_to_M(T,rho)
+elif contamination_model == "random":
+    T = contamination.construct_T_matrix_random(K, epsilon, random_state=seed)
+    M = contamination.convert_T_to_M(T,rho)
+else:
+    print("Unknown contamination (M) model!")
+    sys.stdout.flush()
+    exit(-1)
+
+# Compute the contaminated label proportions
+rho_tilde = np.dot(T, rho)
+
+# Initialize black-box model
+if model_name == 'RFC':
+    black_box = arc.black_boxes.RFC(n_estimators=100)
+elif model_name == 'SVC':
+    black_box = arc.black_boxes.SVC(clip_proba_factor = 1e-5)
+elif model_name == 'NN':
+    black_box = arc.black_boxes.NNet(max_iter=100)
+else:
+    print("Unknown model!")
+    sys.stdout.flush()
+    exit(-1)
+
+
+# Add important parameters to table of results
+header = pd.DataFrame({'data':[data_name], 'num_var':[num_var], 'K':[K],
+                       'signal':[signal], 'n_train':[n_train], 'n_cal':[n_cal],
+                       'epsilon':[epsilon], 'nu':[nu], 'contamination':[contamination_model],
+                       'model_name':[model_name], 'estimate':[estimate], 'seed':[seed]})
+
+# Output file
+outfile_prefix = "exp"+str(exp_num) + "/" + data_name + "_p" + str(num_var)
+outfile_prefix += "_K" + str(K) + "_signal" + str(signal) + "_" + model_name
+outfile_prefix += "_eps" + str(epsilon) + "_nu" + str(nu) + "_" + contamination_model
+outfile_prefix += "_nt" + str(n_train) + "_nc" + str(n_cal) + "_est" + estimate + "_seed" + str(seed)
+print("Output file: {:s}.".format("results/"+outfile_prefix), end="\n")
+sys.stdout.flush()
+
+# Describe the experiment
+def run_experiment(random_state):
+    print("\nRunning experiment in batch {:d}...".format(random_state))
+    sys.stdout.flush()
+
+    # Generate a large data set
+    print("\nGenerating data...", end=' ')
+    sys.stdout.flush()
+    data_distribution.set_seed(random_state+1)
+    X_all, Y_all = data_distribution.sample(n_train+n_cal+n_test)
+    print("Done.")
+    sys.stdout.flush()
+
+    # Separate the test set
+    X, X_test, Y, Y_test = train_test_split(X_all, Y_all, test_size=n_test, random_state=random_state+2)
+
+    # Generate the contaminated labels
+    print("Generating contaminated labels...", end=' ')
+    sys.stdout.flush()
+    contamination_process = contamination.LinearContaminationModel(T, random_state=random_state+3)
+    Yt = contamination_process.sample_labels(Y)
+    print("Done.")
+    sys.stdout.flush()
+
+    # Estimate (if applicable) the label contamination model
+    if estimate=="none":
+        rho_hat = rho
+        rho_tilde_hat = rho_tilde
+        M_hat = M
+    elif estimate=="rho":
+        rho_tilde_hat = estimate_rho(Yt, K)
+        rho_hat = np.dot(M.T, rho_tilde_hat)
+        M_hat = M        
+    else:
+        print("Unknown estimation option!")
+        sys.stdout.flush()
+        exit(-1)
+
+
+    # Apply standard method to corrupted labels (for training)
+    print("Training the predictive model...", end=' ')
+    sys.stdout.flush()
+    method_train = arc.methods.SplitConformal(X, Yt, black_box, K, 0.1, n_cal=n_cal, random_state=random_state)
+    print("Done.")
+    sys.stdout.flush()
+
+    # Extract the pre-trained model
+    black_box_pt = method_train.black_box
+
+    res = pd.DataFrame({})
+    for alpha in [0.1]:
+        #for guarantee in ['lab-cond', 'marginal']:
+        for guarantee in ['marginal']:
+            print("\nSeeking {:s} coverage at level {:.2f}.".format(guarantee, 1-alpha))
+
+            #if guarantee=='lab-cond':
+            #    label_conditional = True
+            #else:
+            label_conditional = False
+
+            # Apply standard method to corrupted labels
+            print("Applying standard method (with model training)...", end=' ')
+            sys.stdout.flush()
+            method_sc = arc.methods.SplitConformal(X, Yt, black_box_pt, K, alpha, n_cal=n_cal, label_conditional=label_conditional,
+                                                   allow_empty=allow_empty, pre_trained=True, random_state=random_state)
+            S_sc = method_sc.predict(X_test)
+            print("Done.")
+            sys.stdout.flush()
+
+
+            # Apply old adaptive method to corrupted labels
+            print("Applying adaptive method...", end=' ')
+            sys.stdout.flush()
+            method_ln = MarginalLabelNoiseConformal(X, Yt, black_box_pt, K, alpha, n_cal=n_cal, epsilon=epsilon, T=T, M=M, rho_tilde=rho_tilde_hat,
+                                            allow_empty=allow_empty, method="old", verbose=False, pre_trained=True, random_state=random_state)
+            S_ln = method_ln.predict(X_test)
+            print("Done.")
+            sys.stdout.flush()
+
+            # Apply optimized adaptive method to corrupted labels
+            print("Applying optimized adaptive method...", end=' ')
+            sys.stdout.flush()
+            method_ln_imp = MarginalLabelNoiseConformal(X, Yt, black_box_pt, K, alpha, n_cal=n_cal, epsilon=epsilon, T=T, rho_tilde=rho_tilde_hat,
+                                            allow_empty=allow_empty, method="improved", optimized=True, verbose=False, pre_trained=True, random_state=random_state)
+            S_ln_imp = method_ln_imp.predict(X_test)
+            print("Done.")
+            sys.stdout.flush()
+
+            # Apply simplified adaptive method to corrupted labels
+            print("Applying simplified adaptive method...", end=' ')
+            sys.stdout.flush()
+            method_ln_imp_simpl = MarginalLabelNoiseConformal(X, Yt, black_box_pt, K, alpha, n_cal=n_cal, epsilon=epsilon, T=T, rho_tilde=rho_tilde_hat,
+                                            allow_empty=allow_empty, method="improved", optimized=False, verbose=False, pre_trained=True, random_state=random_state)
+            S_ln_imp_simpl = method_ln_imp_simpl.predict(X_test)
+            print("Done.")
+            sys.stdout.flush()
+
+            # Apply asymptotic adaptive method to corrupted labels
+            print("Applying asymptotic adaptive method...", end=' ')
+            sys.stdout.flush()
+            method_ln_asy = MarginalLabelNoiseConformal(X, Yt, black_box_pt, K, alpha, n_cal=n_cal, epsilon=epsilon, T=T, rho_tilde=rho_tilde_hat,
+                                            allow_empty=allow_empty, method="asymptotic", verbose=False, pre_trained=True, random_state=random_state)
+            S_ln_asy = method_ln_asy.predict(X_test)
+            print("Done.")
+            sys.stdout.flush()
+
+            # Evaluate methods
+            res_sc = evaluate_predictions(S_sc, X_test, Y_test, K, verbose=False)
+            res_ln = evaluate_predictions(S_ln, X_test, Y_test, K, verbose=False)
+            res_ln_imp = evaluate_predictions(S_ln_imp, X_test, Y_test, K, verbose=False)
+            res_ln_imp_simpl = evaluate_predictions(S_ln_imp_simpl, X_test, Y_test, K, verbose=False)
+            res_ln_asy = evaluate_predictions(S_ln_asy, X_test, Y_test, K, verbose=False)
+
+            # Combine results
+            res_sc['Method'] = "Standard"
+            res_ln['Method'] = "Adaptive"
+            res_ln_imp['Method'] = "Adaptive optimized"
+            res_ln_imp_simpl['Method'] = "Adaptive simplified"
+            res_ln_asy['Method'] = "Asymptotic"
+            
+            res_new = pd.concat([res_sc, res_ln_pes, res_ln_opt])
+            
+            res_new['Guarantee'] = guarantee
+            res_new['Alpha'] = alpha
+            res_new['random_state'] = random_state
+            res = pd.concat([res, res_new])
+
+    print(res)
+
+    return res
+
+# Run all experiments
+results = pd.DataFrame({})
+for batch in np.arange(1,batch_size+1):
+    res = run_experiment(1000*seed+batch-1000)
+    results = pd.concat([results, res])
+
+    # Save results
+    outfile = "results/" + outfile_prefix + ".txt"
+    results_out = pd.concat([header,results], axis=1)
+    results_out.to_csv(outfile, index=False, float_format="%.5f")
+
+print("\nPreview of results:")
+print(results)
+sys.stdout.flush()
+
+print("\nSummary of results:")
+summary = results.groupby(['Alpha', 'Guarantee', 'Method', 'Label']).agg(['mean','std']).reset_index()
+print(summary)
+sys.stdout.flush()
+
+
+print("\nFinished.\nResults written to {:s}\n".format(outfile))
+sys.stdout.flush()
