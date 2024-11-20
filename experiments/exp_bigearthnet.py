a4d157405df18a1b0febd378a0e2802f2e611014
Teresa Bortolotti <tb_214@discovery2.hpc.usc.edu>
2024-11-20 00:52:05 -0800

    Adding experiments

--- experiments/exp_bigearthnet.py
+++ experiments/exp_bigearthnet.py
@@ -52,7 +52,7 @@ seed = 1
 if True:
     print ('Number of arguments:', len(sys.argv), 'arguments.')
     print ('Argument List:', str(sys.argv))
-    if len(sys.argv) != 6:
+    if len(sys.argv) != 7:
         print("Error: incorrect number of parameters.")
         quit()
     sys.stdout.flush()
