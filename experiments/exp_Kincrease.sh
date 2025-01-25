9e41dab8b9433396a339abc28090bc7f75238634
tbortolotti <teresa.bortolotti1@gmail.com>
2025-01-25 18:43:44 +0100

    Exp on K increase

--- experiments/exp_Kincrease.sh
+++ experiments/exp_Kincrease.sh
@@ -6,4 +6,4 @@ conda activate default
 
 export OPENBLAS_NUM_THREADS=1
 
-python3 exp_Kincrease.py $1 $2 $3 $4 $5 $6 $7 $8 $9 "${10}" "${11}" "${12}" "${13}"
+python3 exp_Kincrease.py $1 $2 $3 $4 $5 $6 $7 $8 $9 "${10}" "${11}" "${12}"
